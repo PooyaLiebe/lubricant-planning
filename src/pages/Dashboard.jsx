2c9f4467a33b0cee41454b5f614b79a95d5c3495
sHRLo <whitebeard@gmail.com>
2025-02-21 19:15:52 +0330

    modify

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,22 +2,13 @@
 import React, { useEffect, useState } from "react";
 
 const Dashboard = () => {
-  const [items, setItems] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("http://localhost:5000/dashboard");
-      const data = await res.json();
-      console.log(data);
-    };
-    fetchData();
-  }, []);
   return (
     <>
-      {items.map((i) => (
-        <p>
-          {i.lubricantname},{(i, type)}
-        </p>
-      ))}
+      <div className="flex items-center justify-center h-full bg-amber-200">
+        <div className="flex items-center justify-center h-full bg-amber-300">
+          dashboard
+        </div>
+      </div>
     </>
   );
 };
